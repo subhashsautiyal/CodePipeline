886298bfb21e6816e8e665f05b69af29faf81494
subhashsautiyal <68697760+subhashsautiyal@users.noreply.github.com>
2024-12-30 23:05:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Tech2Towards :-)
+          Edit <code>src/App.js</code> Tech2Towards  30-dec-2024 :-)
         </p>
         <a
           className="App-link"
